1674bc9f41449a54d2651ef0a23427ade7b485f6
郑旭的个人主页 <38687093+zhengxu110sc@users.noreply.github.com>
2020-05-16 22:40:41 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,106 @@
+<?php return array (
+  'site_name' => 'OneIndex',
+  'password' => 'oneindex',
+  'style' => 'nexmoe',
+  'onedrive_root' => '/',
+  'cache_type' => 'filecache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+    ),
+    'video' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => '11/3U0:l?vXLH?W=UoFh9PaX98g1MiiV',
+  'client_id' => 'd6858363-597d-489a-b3e8-2c4b9855768c',
+  'redirect_uri' => 'https://qinyihao.herokuapp.com/',
+  'refresh_token' => 'OAQABAAAAAABTYj0VqP05TZ6xV9yGkWlGeGgTja-tLcowNbEo6rdiVMPId_NgPjhStwzrUekLTafIfVScvG_jiPnDa3exbtzlKtQjjDOsvu112IwYhHkl8EayGmqPM_ldcxzhrtjH6Dw-s8ULpnTsUoX3u4gQwQgGIL2wM4-wu2A9rW9c8ZVOQ7x5PlNu_1w0drrggEODD5eVOP4jHh7fIsu1u3r1UxNM5U6fS0ryL-Jj-_6SQHZm2QN99wb5T36D7UdMJ5OPrkTQFP36APyCbdWq2YGqHe_9v0JI8kw29LONBVVZGSzeIcak3GgcSl410HdyMhaPoLhR3OONMHM46t9xCfFZdjiqPINII7jrvsCfX6PDba9ajw0TL2OtN2C-vwlF-kYjcvAJLLqr6IjqgkAaKYRRRphBqFgsiDlkWHgn75jqcb1vQCQbpLDWz9eAtwUKVp7mCuQNHINpJ8xhATRSuIo7LpCTJxWyjZeBt0qpNBh9Nt0sP5HXBGOPSL314U0oAQaZcRdx_RlZhkpbhg8jvsfLAqL29ehjUw5_aG-jnZXryzvLNPdIbDuwGKJsuZQ3ZFNBUwzRj7P07LqRNF8ph43Reb_onvVAlEiM6_60JEjK86NUAsbqG0EcIo6Ot7K1zFos-PvGr4gZBYpmAAXmXu2-3YZYFlJHFLjHPfB2DtDOot8-ullsSn1aKVzSCrd1uaCtBI4QDxFI_H-gf8rgf2YhFhnydm7gZwmVQ6Fb0BDg_aH2n83wrddRoXleBrubTb5A_KksrdzurKekwztaiIAq_GwbmZpcvqI4hqVUVOEjUUQUtANCfYnwvt8h2OKrvFQbwqpK8vg9-v0jwfk-SdLbhhWgIAA',
+  'onedrive_hide' => '',
+  'onedrive_hotlink' => '',
+);
